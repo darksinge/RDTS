2ad9fd7aca7d1cd6be3d8646a5f377a00411f1aa
Craig Blackburn <craig.r.blackburn@gmail.com>
2022-03-30 23:42:07 -0600

    wip

--- internal/saga.go
+++ internal/saga.go
@@ -3,9 +3,22 @@ package rdts
 type Saga struct {
 	id           string
 	members      []*Service
-	stateMachine StateMachine
+	stateMachine stateMachine
 }
 
 func (s *Saga) Members() []*Service {
-	events := []*Event{s.stateMachine.head}
+	events := []*Event{}
+
+	var event *Event
+	event = &s.stateMachine.head.event
+	for event != nil {
+		events = append(events, event)
+		event = event
+	}
+
+	services := []*Service{}
+	for _, e := range events {
+		services = append(services, e.owner)
+	}
+	return services
 }
